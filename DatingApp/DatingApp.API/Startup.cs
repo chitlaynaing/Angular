185efd907e5dd710b199ca824b90fe581729ca83
Chit Lay Naing <chitlaynaing@gmail.com>
2018-10-11 21:17:50 +1100

    Added Dbcontext

--- DatingApp/DatingApp.API/Startup.cs
+++ DatingApp/DatingApp.API/Startup.cs
@@ -25,6 +25,7 @@ namespace DatingApp.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<DataContext>(x=> x.UseSqlite("Connectionstring"));
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
