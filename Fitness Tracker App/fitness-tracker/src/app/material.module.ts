5632391a4e7145bf00f7f6ba244ea92bde843af2
Chit Naing <chitlaynaing@gmail.com>
2019-03-31 16:16:13 +1100

    added signup forms and other templates

--- Fitness Tracker App/fitness-tracker/src/app/material.module.ts
+++ Fitness Tracker App/fitness-tracker/src/app/material.module.ts
@@ -1,17 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MatButtonModule, MatIconModule } from '@angular/material';
+import { MatButtonModule, MatIconModule, MatFormFieldModule, MatInputModule } from '@angular/material';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
     MatButtonModule,
-    MatIconModule
+    MatIconModule,
+    MatFormFieldModule,
+    MatInputModule
   ],
   exports:[
     MatButtonModule,
-    MatIconModule
+    MatIconModule,
+    MatFormFieldModule,
+    MatInputModule
   ]
 })
 export class MaterialModule { }
