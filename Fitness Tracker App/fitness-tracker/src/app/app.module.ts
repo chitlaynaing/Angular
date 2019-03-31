a9b543e3852deb64993872231a6a54ef613354b1
Chit Naing <chitlaynaing@gmail.com>
2019-03-31 13:49:19 +1100

    added fitness tracker app

---
+++ Fitness Tracker App/fitness-tracker/src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { MaterialModule } from './material.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
